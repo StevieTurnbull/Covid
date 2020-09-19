659adb33711a46966a1943a4768b2a8104f086ca
Stevie Turnbull <stevie_turnbull@live.co.uk>
2020-09-19 21:46:44 +0100

    Start moving towards multi-country support

---
+++ countries.js
@@ -0,0 +1,225 @@
+// Master data source for countries and their regions
+// - also defines the data source
+
+const COUNTRYDATA = [
+    {
+        name: "United Kingdom",
+        dataSource: "https://api.apify.com/v2/datasets/K1mXdufnpvr53AFk6/items?format=json&clean=1",
+        countryDataKeys: [ "infected", "totalInfected" ], 
+        regions: [
+            { displayName: "England", dataKeys: [ "england", "englandConfirmed" ] },
+            { displayName: "Scotland", dataKeys: [ "scottland", "scottlandConfirmed" ] },
+            { displayName: "Wales", dataKeys: [ "wales", "walesConfirmed" ] },
+            { displayName: "N. Ireland", dataKeys: [ "ireland", "northernIrelandConfirmed" ] }
+        ]
+    },
+    {
+        name: "Netherlands",
+        dataSource: "https://api.apify.com/v2/datasets/jr5ogVGnyfMZJwpnB/items?format=json&clean=1",
+        countryDataKeys: [ "infected" ],
+        regions: [
+            { displayName: "South Holland", dataKeys: [ "Zuid-Holland" ] },
+            { displayName: "North Brabant", dataKeys: [ "Noord-Brabant" ] },
+            { displayName: "North Holland", dataKeys: [ "Noord-Holland" ] },
+            { displayName: "Gelderland", dataKeys: [ "Gelderland" ] },
+            { displayName: "Limburg", dataKeys: [ "Limburg" ] },
+            { displayName: "Utrecht", dataKeys: [ "Utrecht" ] },
+            { displayName: "Overijssel", dataKeys: [ "Overijssel" ] },
+            { displayName: "Aruba", dataKeys: [ "Aruba" ] },
+            { displayName: "Flevoland", dataKeys: [ "Flevoland" ] },
+            { displayName: "Zeeland", dataKeys: [ "Zeeland" ] },
+            { displayName: "Friesland", dataKeys: [ "Friesland" ] },
+            { displayName: "Unknown", dataKeys: [ "Unknown" ] },
+            { displayName: "Drenthe", dataKeys: [ "Drenthe" ] },
+            { displayName: "Groningen", dataKeys: [ "Groningen" ] },
+            { displayName: "Saint Martin", dataKeys: [ "Sint Maarten" ] },
+            { displayName: "Curacao", dataKeys: [ "Curacao" ] },
+            { displayName: "Bonaire, Sint Eustatius and Saba", dataKeys: [ "Bonaire, Sint Eustatius and Saba" ] }
+        ]
+    }
+];
+
+// Process the regional data from the raw data block...
+
+function getRegionsDataBlock(rawBlock) {
+
+    let eng = rawBlock["england"];
+    if (!eng) {
+        eng = rawBlock["englandConfirmed"];
+    }
+
+    let sco = rawBlock["scottland"];
+    if (!sco) {
+        sco = rawBlock["scottlandConfirmed"];
+    }
+
+    let wal = rawBlock["wales"];
+    if (!wal) {
+        wal = rawBlock["walesConfirmed"];
+    }
+
+    let nir = rawBlock["ireland"];
+    if (!nir) {
+        nir = rawBlock["northernIrelandConfirmed"];
+    }
+
+    if (!eng || !sco || !wal || !nir) return null;
+
+    return { england: eng,
+             scotland: sco,
+             wales: wal,
+             nireland: nir };
+}
+
+// Process the country data from the raw data block...
+
+function getCountryDataBlock(rawBlock) {
+
+    let totalInfected = rawBlock["infected"];
+    if (!totalInfected) {
+        totalInfected = rawBlock["totalInfected"];
+    }
+
+    if (!totalInfected) {
+        // Try to get the whole UK value from the summed regional values...
+        let reg = getRegionsDataBlock(rawBlock);
+        if (reg) {
+            return { totalInfected: reg.england + reg.scotland + reg.wales + reg.nireland }
+        }
+
+        return null;
+    }
+
+    return { date: null,
+             totalInfected: totalInfected };
+}
+
+// Add country and regional data into the supplied datastore...
+
+function addCountryAndRegionalData(ds, blockDate, rawBlock) {
+
+    // Get UK data...
+    let countryData = getCountryDataBlock(rawBlock);
+    if (countryData) {
+        countryData.date = blockDate;
+
+        // Make sure we only have the most recent data for a particular day...
+        let len = ds.cleanCountryDataInfected.length;
+        if (len > 0 && ds.cleanCountryDataInfected[len - 1].date.getTime() == blockDate.getTime()) {
+            ds.cleanCountryDataInfected.pop();
+        }
+
+        ds.cleanCountryDataInfected.push(countryData);
+    }
+
+    // Get regional data...
+    let regData = getRegionsDataBlock(rawBlock);
+    if (regData) {
+        regData.date = blockDate;
+
+        // Make sure we only have the most recent data for a particular day...
+        let len = ds.cleanRegDataInfected.length;
+        if (len > 0 && ds.cleanRegDataInfected[len - 1].date.getTime() == blockDate.getTime()) {
+            ds.cleanRegDataInfected.pop();
+        }
+
+        ds.cleanRegDataInfected.push(regData);
+    }
+
+    // This is a workaround to ensure display of regional data
+    // as there are only a few good data points from the chosen provider.
+    // So we produce a VERY rough estimate based on UK population...
+    // Source: https://en.wikipedia.org/wiki/Countries_of_the_United_Kingdom_by_population
+    if (countryData && !regData) {
+        let countryValue = countryData.totalInfected;
+        let block = { date: blockDate,
+                      england: Math.ceil(countryValue * (84.3 / 100.0)),        // 84.3% of UK population
+                      scotland: Math.ceil(countryValue * (8.2 / 100.0)),        // 8.2%
+                      wales: Math.ceil(countryValue * (4.7 / 100.0)),           // 4.7%
+                      nireland: Math.ceil(countryValue * (2.8 / 100.0)) };      // 2.8%
+
+        // Make sure we only have the most recent data for a particular day...
+        let len = ds.cleanRegDataInfected.length;
+        if (len > 0 && ds.cleanRegDataInfected[len - 1].date.getTime() == blockDate.getTime()) {
+            ds.cleanRegDataInfected.pop();
+        }
+
+        ds.cleanRegDataInfected.push(block);
+    }
+}
+
+// Generate single stream for country-wide data from the supplied datastore...
+
+function generateCountryDataSet(ds, startDate, endDate) {
+
+    // If we've got no actual data just return filler...
+    if (ds.cleanCountryDataInfected.length === 0) {
+        return {
+            xAxisData: [ getChartDateString(startDate), getChartDateString(endDate) ],
+            yAxisData: [ 0, 0 ]
+        }
+    }
+
+    let axesData = {
+        xAxisData: [],
+        yAxisData: []
+    }
+
+    ds.cleanCountryDataInfected.forEach(function (block) {
+
+        if (block.date.getTime() < startDate.getTime()) return;
+        if (block.date.getTime() > endDate.getTime()) return;
+
+        axesData.xAxisData.push(getChartDateString(block.date));
+        axesData.yAxisData.push(block.totalInfected);
+    });
+
+    return axesData;
+}
+
+// Generate multiple streams for country-wide data from the supplied datastore...
+
+function generateRegionDataSets(ds, startDate, endDate, regions) {
+
+    // If we've got no actual data just return filler...
+    if (ds.cleanRegDataInfected.length === 0) {
+        return {
+            xAxisData: [ getChartDateString(startDate), getChartDateString(endDate) ],
+            displayLegend: false,
+            data: [ { label: "None", data: [ 0, 0 ], borderColor: ["black"] } ]
+        }
+    }
+
+    let axesData = {
+        xAxisData: [],
+        displayLegend: true,
+        data: []
+    }
+
+    let streams = [ { label: "England", data: [], borderColor: "blue" },
+                    { label: "Wales", data: [], borderColor: "darkRed" },
+                    { label: "Scotland", data: [], borderColor: "orange" },
+                    { label: "N. Ireland", data: [], borderColor: "green" } ];
+
+    ds.cleanRegDataInfected.forEach(function (block) {
+
+        if (block.date.getTime() < startDate.getTime()) return;
+        if (block.date.getTime() > endDate.getTime()) return;
+
+        axesData.xAxisData.push(getChartDateString(block.date));
+
+        if (regions.england) streams[0].data.push(block.england);
+        if (regions.wales) streams[1].data.push(block.wales);
+        if (regions.scotland) streams[2].data.push(block.scotland);
+        if (regions.nireland) streams[3].data.push(block.nireland);
+    });
+
+    streams.forEach(function(s) {
+
+        if (s.data.length === 0) return;
+
+        axesData.data.push(s);
+    });
+
+    return axesData;
+}
--- data_processing.js
+++ data_processing.js
@@ -9,8 +9,9 @@ let dataStore = {
 
     startDate: new Date("January 1, 2020 00:00:00"),
     endDate: new Date(),
-    cleanUKDataInfected: [],
-    cleanRegDataInfected: []
+    cleanCountryDataInfected: [],
+    cleanRegDataInfected: [],
+    selectedCountry: 0
 }
 
 let tmChart = null;
@@ -53,57 +54,6 @@ function getDateTimeString(d) {
            d.toTimeString().split(" ")[0];
 }
 
-function getRegDataBlock(rawBlock) {
-
-    let eng = rawBlock["england"];
-    if (!eng) {
-        eng = rawBlock["englandConfirmed"];
-    }
-
-    let sco = rawBlock["scottland"];
-    if (!sco) {
-        sco = rawBlock["scottlandConfirmed"];
-    }
-
-    let wal = rawBlock["wales"];
-    if (!wal) {
-        wal = rawBlock["walesConfirmed"];
-    }
-
-    let nir = rawBlock["ireland"];
-    if (!nir) {
-        nir = rawBlock["northernIrelandConfirmed"];
-    }
-
-    if (!eng || !sco || !wal || !nir) return null;
-
-    return { england: eng,
-             scotland: sco,
-             wales: wal,
-             nireland: nir };
-}
-
-function getUKDataBlock(rawBlock) {
-
-    let uk = rawBlock["infected"];
-    if (!uk) {
-        uk = rawBlock["totalInfected"];
-    }
-
-    if (!uk) {
-        // Try to get the whole UK value from the summed regional values...
-        let reg = getRegDataBlock(rawBlock);
-        if (reg) {
-            return { unitedKingdom: reg.england + reg.scotland + reg.wales + reg.nireland }
-        }
-
-        return null;
-    }
-
-    return { date: null,
-             unitedKingdom: uk };
-}
-
 function getDate(rawBlock) {
 
     let date = rawBlock["lastUpdatedAtApify"];
@@ -136,54 +86,7 @@ function buildDataStore(rawData) {
             if (date > endDate) endDate = date;
         }
 
-        // Get UK data...
-        let ukData = getUKDataBlock(rawBlock);
-        if (ukData) {
-            ukData.date = date;
-
-            // Make sure we only have the most recent data for a particular day...
-            let len = dataStore.cleanUKDataInfected.length;
-            if (len > 0 && dataStore.cleanUKDataInfected[len - 1].date.getTime() == date.getTime()) {
-                dataStore.cleanUKDataInfected.pop();
-            }
-
-            dataStore.cleanUKDataInfected.push(ukData);
-        }
-
-        // Get regional data...
-        let regData = getRegDataBlock(rawBlock);
-        if (regData) {
-            regData.date = date;
-
-            // Make sure we only have the most recent data for a particular day...
-            let len = dataStore.cleanRegDataInfected.length;
-            if (len > 0 && dataStore.cleanRegDataInfected[len - 1].date.getTime() == date.getTime()) {
-                dataStore.cleanRegDataInfected.pop();
-            }
-
-            dataStore.cleanRegDataInfected.push(regData);
-        }
-
-        // This is a workaround to ensure display of regional data
-        // as there are only a few good data points from the chosen provider.
-        // So we produce a VERY rough estimate based on UK population...
-        // Source: https://en.wikipedia.org/wiki/Countries_of_the_United_Kingdom_by_population
-        if (ukData && !regData) {
-            let ukValue = ukData.unitedKingdom;
-            let block = { date: date,
-                          england: Math.ceil(ukValue * (84.3 / 100.0)),        // 84.3% of UK population
-                          scotland: Math.ceil(ukValue * (8.2 / 100.0)),        // 8.2%
-                          wales: Math.ceil(ukValue * (4.7 / 100.0)),           // 4.7%
-                          nireland: Math.ceil(ukValue * (2.8 / 100.0)) };      // 2.8%
-
-            // Make sure we only have the most recent data for a particular day...
-            let len = dataStore.cleanRegDataInfected.length;
-            if (len > 0 && dataStore.cleanRegDataInfected[len - 1].date.getTime() == date.getTime()) {
-                dataStore.cleanRegDataInfected.pop();
-            }
-
-            dataStore.cleanRegDataInfected.push(block);
-        }
+        addCountryAndRegionalData(dataStore, date, rawBlock);
     });
 
     // If no data, leave start / end dates as before...
@@ -193,39 +96,12 @@ function buildDataStore(rawData) {
     dataStore.endDate = endDate;
 }
 
-function generateUKDataSet(startDate, endDate) {
-
-    // If we've got no actual data just return filler...
-    if (dataStore.cleanUKDataInfected.length === 0) {
-        return {
-            xAxisData: [ getChartDateString(startDate), getChartDateString(endDate) ],
-            yAxisData: [ 0, 0 ]
-        }
-    }
-
-    let axesData = {
-        xAxisData: [],
-        yAxisData: []
-    }
-
-    dataStore.cleanUKDataInfected.forEach(function (block) {
-
-        if (block.date.getTime() < startDate.getTime()) return;
-        if (block.date.getTime() > endDate.getTime()) return;
-
-        axesData.xAxisData.push(getChartDateString(block.date));
-        axesData.yAxisData.push(block.unitedKingdom);
-    });
-
-    return axesData;
-}
-
-function rebuildUKChart() {
+function rebuildCountryChart() {
 
     let startDate = $("#tm_date_from").datepicker("getDate");
     let endDate = $("#tm_date_to").datepicker("getDate");
 
-    let dataSet = generateUKDataSet(startDate, endDate);
+    let dataSet = generateCountryDataSet(dataStore, startDate, endDate);
 
     let ctx = $("#tm_chart")[0].getContext("2d");
 
@@ -252,52 +128,7 @@ function rebuildUKChart() {
     });
 }
 
-function generateRegDataSets(startDate, endDate, regions) {
-
-    // If we've got no actual data just return filler...
-    if (dataStore.cleanUKDataInfected.length === 0) {
-        return {
-            xAxisData: [ getChartDateString(startDate), getChartDateString(endDate) ],
-            displayLegend: false,
-            data: [ { label: "None", data: [ 0, 0 ], borderColor: ["black"] } ]
-        }
-    }
-
-    let axesData = {
-        xAxisData: [],
-        displayLegend: true,
-        data: []
-    }
-
-    let streams = [ { label: "England", data: [], borderColor: "blue" },
-                    { label: "Wales", data: [], borderColor: "darkRed" },
-                    { label: "Scotland", data: [], borderColor: "orange" },
-                    { label: "N. Ireland", data: [], borderColor: "green" } ];
-
-    dataStore.cleanRegDataInfected.forEach(function (block) {
-
-        if (block.date.getTime() < startDate.getTime()) return;
-        if (block.date.getTime() > endDate.getTime()) return;
-
-        axesData.xAxisData.push(getChartDateString(block.date));
-
-        if (regions.england) streams[0].data.push(block.england);
-        if (regions.wales) streams[1].data.push(block.wales);
-        if (regions.scotland) streams[2].data.push(block.scotland);
-        if (regions.nireland) streams[3].data.push(block.nireland);
-    });
-
-    streams.forEach(function(s) {
-
-        if (s.data.length === 0) return;
-
-        axesData.data.push(s);
-    });
-
-    return axesData;
-}
-
-function rebuildRegChart() {
+function rebuildRegionsChart() {
 
     let startDate = $("#reg_date_from").datepicker("getDate");
     let endDate = $("#reg_date_to").datepicker("getDate");
@@ -309,7 +140,7 @@ function rebuildRegChart() {
         nireland: $("#nireland_checkbox").is(":checked")
     }
 
-    let dataSets = generateRegDataSets(startDate, endDate, regions);
+    let dataSets = generateRegionDataSets(dataStore, startDate, endDate, regions);
 
     let ctx = $("#reg_chart")[0].getContext("2d");
 
@@ -336,9 +167,9 @@ function rebuildRegChart() {
     });
 }
 
-function startLoading() {
+function startLoading(countryName) {
 
-    $("#status_panel_text").text("Retrieving data");
+    $("#status_panel_text").text("Retrieving " + countryName + " data");
     $("#loader_1").css("visibility", "visible");
     $("#refresh_button").css("pointer-events", "none");
 }
@@ -352,26 +183,26 @@ function stopLoading(msg) {
 
 function retrieveAndProcessData() {
 
-    dataStore.cleanUKDataInfected = [];
+    dataStore.cleanCountryDataInfected = [];
     dataStore.cleanRegDataInfected = [];
 
-    startLoading();
+    startLoading(COUNTRYDATA[dataStore.selectedCountry].name);
     updateDatePickers();
 
-    rebuildUKChart();
-    rebuildRegChart();
+    rebuildCountryChart();
+    rebuildRegionsChart();
 
     let src = "https://api.apify.com/v2/datasets/K1mXdufnpvr53AFk6/items?format=json&clean=1";
 
     $.getJSON(src, function (rawData) {
 
-        stopLoading("Last updated " + getDateTimeString(new Date(Date.now())));
+        stopLoading(COUNTRYDATA[dataStore.selectedCountry].name + ": last updated " + getDateTimeString(new Date(Date.now())));
         buildDataStore(rawData);
 
         updateDatePickers();
 
-        rebuildUKChart();
-        rebuildRegChart();
+        rebuildCountryChart();
+        rebuildRegionsChart();
     })
-    .fail(function () { stopLoading("Failed to retrieve data from API"); })
+    .fail(function () { stopLoading("Failed to retrieve " + COUNTRYDATA[dataStore.selectedCountry].name + " data from API"); })
 }
